cecb6203080f33aec078ded874b8c6bf123a36cf
mark <markpackham1@gmail.com>
2021-08-07 16:13:56 +0100

    add strings, floats and ints, https://youtu.be/m1Uy0WQ2Xns?t=3

--- main.go
+++ main.go
@@ -5,18 +5,42 @@ import "fmt"
 func main() {
 
 	// strings
-	var nameOne string = "Hello"
-	var nameTwo = "Goodbye"
-	var nameThree string
-	
-	fmt.Println(nameOne, nameTwo, nameThree)
+	// var nameOne string = "Hello"
+	// var nameTwo = "Goodbye"
+	// var nameThree string
 
-	nameOne = "Jim"
-	nameThree = "Bob"
+	// fmt.Println(nameOne, nameTwo, nameThree)
 
-	fmt.Println(nameOne, nameTwo, nameThree)
+	// nameOne = "Jim"
+	// nameThree = "Bob"
 
-	nameFour := "Emily"
+	// fmt.Println(nameOne, nameTwo, nameThree)
 
-	fmt.Println(nameOne, nameTwo, nameThree, nameFour)
+	// nameFour := "Emily"
+
+	// fmt.Println(nameFour)
+
+	// ints
+	// var ageOne int = 20
+	// var ageTwo int = 30
+	// ageThree := 40
+
+	// fmt.Println(ageOne, ageTwo, ageThree)
+
+	// bits & memory
+	// var numOne int8 = 25
+	// var numTwo = -12999999
+	// var numThree uint = 255
+
+	// fmt.Println(numOne, numTwo, numThree)
+
+
+	// floats, unlike ints you must provide a number alongside them, use float64 when memory is no real issue
+
+	var scoreOne float32 = -0.025
+	var scoreTwo float64 = 9192.9999129
+	scoreThree := 1.7
+
+	fmt.Println(scoreOne, scoreTwo, scoreThree)
+
 }

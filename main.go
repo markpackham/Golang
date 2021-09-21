7de9de4a2ef69d736492a8e4b1895d55f83964b6
mark <markpackham1@gmail.com>
2021-09-21 09:26:18 +0100

    demo memory address, https://youtu.be/4B2rwYvuiBo?t=186

--- main.go
+++ main.go
@@ -1,36 +1,32 @@
 package main
 
-import(
-	"fmt"
-)
+import "fmt"
 
-func updateName(x string) string{
+func updateName(x string){
 	x = "wedge"
-	return x
-}
-
-// effects variables who you can target in memory to change
-func updateMenu(y map[string]float64) {
-	y["coffee"] = 2.99
 }
 
 func main(){
-	// group A types -> strings, ints, bools, floats, arrays, structs
-	// non-pointer wrapper values
+
 	name := "tifa"
 
 	updateName(name)
 
+	fmt.Println("memory address of name is: ", &name)
+
+	m := &name
+
 	fmt.Println(name)
-
-	// group B types -> slices, maps, functions
-	// pointer wrapper values
-	menu := map[string]float64{
-		"pie":       5.95,
-		"ice cream": 3.99,
-	}
-
-	updateMenu(menu)
-	fmt.Println(menu)
-
 }
+
+
+
+
+/*
+Memory addresses
+|--name---|----m----|
+|  0x001  |  0x002  |
+|---------|---------|
+| "tifa"  | p0x001  |
+|---------|---------|
+*/

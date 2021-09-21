932c0ee8b963b1a49feb41345d282214de211248
mark <markpackham1@gmail.com>
2021-09-21 08:28:28 +0100

    add maps, https://youtu.be/LBLN4Wu5U4w?t=2
    Key Value pair holders that only handle identical types

--- main.go
+++ main.go
@@ -1,21 +1,42 @@
 package main
 
+// a "map" is like a dictionary in python with key value pairs
+// in a map it must be all the same type, you can't just chuck anything into it
+
 import(
 	"fmt"
 )
 
-var score = 99.5
-
-func main() {
-	sayHello("mario")
-
-	for _, v := range points{
-		fmt.Println(v)
+func main(){
+	menu := map[string]float64{
+		"soup": 4.99,
+		"pie": 7.99,
+		"salad": 6.99,
+		"toffee": 3.55,
 	}
 
-	showScore()
+	fmt.Println(menu)
+	fmt.Println(menu["pie"])
+
+	// looping maps with key and value (you must use "k" and "v")
+	for k, v := range menu{
+		fmt.Println(k, "-", v)
+	}
+
+	// ints as key type
+	phonebook := map[int]string{
+		267584967: "mario",
+		984759373: "luigi",
+		845775485: "peach",
+	}
+
+	fmt.Println(phonebook)
+	fmt.Println(phonebook[267584967])
+
+	phonebook[984759373] = "wario"
+	fmt.Println(phonebook)
+
+	phonebook[845775485] = "toad"
+	fmt.Println(phonebook)
+
 }
-
-
-// To run this file you must also run ones it is using
-// go run main.go greetings.go 

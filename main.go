18309c96a624bdfaa5eb9eedeb7418cbf0910b0c
mark <markpackham1@gmail.com>
2021-09-21 14:44:27 +0100

    use pointer to change value via memory location, https://youtu.be/zfMhSFpBSaw?t=2

--- main.go
+++ main.go
@@ -2,20 +2,24 @@ package main
 
 import "fmt"
 
-func updateName(x string){
-	x = "wedge"
+func updateName(x *string){
+	*x = "wedge"
 }
 
 func main(){
 
 	name := "tifa"
 
-	updateName(name)
+	// updateName(name)
 
 	fmt.Println("memory address of name is: ", &name)
 
 	m := &name
+	// fmt.Println("memory address", m)
+	// fmt.Println("value at memory address", *m)
 
+	fmt.Println(name)
+	updateName(m)
 	fmt.Println(name)
 }
 

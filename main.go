d4f8c7cf0194b0b7212c92c239326e224e6cbbbb
mark <markpackham1@gmail.com>
2021-09-21 08:15:04 +0100

    add package score, https://youtu.be/v3RodjH1i6c?t=2

--- main.go
+++ main.go
@@ -1,38 +1,21 @@
 package main
 
-import (
+import(
 	"fmt"
-	"strings"
 )
 
-func getInitials(name string) (string, string) {
-
-	newString := strings.ToUpper(name)
-	names := strings.Split(newString, " ")
-
-	var initials []string
-	// don't want index but do want value so _, V
-	for _, v := range names {
-		initials = append(initials, v[:1])
-	}
-
-	if len(initials) > 1 {
-		return initials[0], initials[1]
-	}
-
-	// if you only have one name return this
-	return initials[0], "_"
-}
+var score = 99.5
 
 func main() {
-	firstName1, surName1 := getInitials("Squall Leonheart")
-	fmt.Println(firstName1, surName1)
+	sayHello("mario")
 
-	// we can only handle 2 params so "Ardyn Lucis Caelum" would just return "AL" so "C" wouldn't be returned
-	fn2, sn2 := getInitials("Ardyn Lucis Caelum")
-	fmt.Println(fn2, sn2)
+	for _, v := range points{
+		fmt.Println(v)
+	}
 
-	// Yuna give "Y _"
-	fn3, sn3 := getInitials("Yuna")
-	fmt.Println(fn3, sn3)
+	showScore()
 }
+
+
+// To run this file you must also run ones it is using
+// go run main.go greetings.go 

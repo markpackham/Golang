fb896aa5fae025f65ac8704f1d602b189a8404b5
mark <markpackham1@gmail.com>
2021-09-21 16:23:22 +0100

    give user options choice, https://youtu.be/l6zlJFJst8o?t=6

--- main.go
+++ main.go
@@ -7,12 +7,21 @@ import (
 	"strings"
 )
 
+func getInput(prompt string, r *bufio.Reader) (string, error){
+	fmt.Print(prompt)
+	input, err := r.ReadString('\n')
+
+	return strings.TrimSpace(input), err
+}
+
 func createBill() bill{
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Print("Create a new bill name: ")
-	name, _ := reader.ReadString('\n')
-	name = strings.TrimSpace(name)
+	// fmt.Print("Create a new bill name: ")
+	// name, _ := reader.ReadString('\n')
+	// name = strings.TrimSpace(name)
+
+	name, _ := getInput("Create a new bill name: ", reader)
 
 	b := newBill(name)
 	fmt.Println("Created the bill - ", b.name)
@@ -20,10 +29,17 @@ func createBill() bill{
 	return b
 }
 
+func promptOptions(b bill){
+	reader := bufio.NewReader(os.Stdin)
+
+	opt, _ := getInput("Choose option (a - add item, s - save bill, t - add tip): ", reader)
+	fmt.Println(opt)
+
+}
+
 func main(){
 	mybill := createBill()
-
-	fmt.Println(mybill)
+	promptOptions(mybill)
 }
 
 
